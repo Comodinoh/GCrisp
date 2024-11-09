fc26dc48a7df858d5005b51749852fa876412c40
comodinoh <plumino3124@gmail.com>
2024-11-09 14:02:21 +0200

    dumb

---
+++ src/core.cpp
@@ -0,0 +1,11 @@
+#include "core.h"
+#include <sys/types.h>
+
+struct GCrispCoreInfo* gcrisp_core_start(char* game_name, ushort game_version)
+{
+  GCrispCoreInfo* core_info = (GCrispCoreInfo*) malloc(sizeof(GCrispCoreInfo));
+  core_info->game_name = game_name;
+  core_info->game_version = game_version;
+  return core_info;
+}
+
