d852598b19f52cd1233e00e7e25feee9858c63a4
comodinoh <plumino3124@gmail.com>
2024-11-09 14:10:33 +0200

    test

--- src/core.cpp
+++ src/core.cpp
@@ -6,6 +6,7 @@ struct GCrispCoreInfo* gcrisp_core_start(char* game_name, ushort game_version)
   GCrispCoreInfo* core_info = (GCrispCoreInfo*) malloc(sizeof(GCrispCoreInfo));
   core_info->game_name = game_name;
   core_info->game_version = game_version;
+
   return core_info;
 }
 
