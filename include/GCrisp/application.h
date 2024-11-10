1fda8e3e44379b099eaf48a4e1332b455d0053f8
comodinoh <plumino3124@gmail.com>
2024-11-10 22:10:39 +0200

    create application class

---
+++ include/GCrisp/application.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+namespace gcrisp {
+
+class Application
+{
+public:
+  Application();
+  virtual ~Application();
+
+  void Run();
+};
+
+
+}
+
---
+++ src/application.cpp
@@ -0,0 +1,18 @@
+#include <GCrisp/application.h>
+
+namespace gcrisp{
+
+Application::Application()
+{
+}
+
+Application::~Application()
+{
+}
+
+void Application::Run()
+{
+  while(true);
+}
+
+}
